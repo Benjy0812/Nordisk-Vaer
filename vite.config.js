5a239ccf030264b7cd066bbe2f262b8b7f849511
Benjy <108727379+Benjy0812@users.noreply.github.com>
2026-08-21 23:37:48 +0200

    Update base name for website paths

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   // Uncomment the line below if you want to deploy to GitHub Pages or a subdirectory
-  //base: '/NettsideNordKlima/',
+  base: '/Nordisk-Vaer/',
   // Force full page reload on CSS changes
   plugins: [
     tailwindcss(),
